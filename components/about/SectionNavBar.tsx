efe57cdac0056b1d1fc66d48256443e070988f1a
Sambatra <andriamihajasambatra@gmail.com>
2024-04-24 09:44:26 +0300

    🎨 Section Navbar > Improve style

--- components/about/SectionNavBar.tsx
+++ components/about/SectionNavBar.tsx
@@ -62,7 +62,8 @@ const SectionNavBar = (props: ISectionNavBar) => {
             href={item.url}
             onClick={() => handleClick(item.title)}
             className={`block px-4 py-2 text-dark dark:text-light hover:bg-light hover:dark:bg-dark ${
-              item.title == active && "bg-light dark:bg-dark"
+              item.title == active &&
+              "bg-light dark:bg-dark font-extrabold underline"
             }`}
           >
             {t(`${item.title}`)}
