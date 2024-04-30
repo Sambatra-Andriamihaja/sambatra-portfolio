9af66e3563be835a5ff4c5106810681a69623ecb
Sambatra <andriamihajasambatra@gmail.com>
2024-04-30 11:29:41 +0300

    🎨 Language Switcher > Add pointer effect on cursor

--- components/sub/language-switcher/LanguageSwitcher.tsx
+++ components/sub/language-switcher/LanguageSwitcher.tsx
@@ -43,7 +43,7 @@ const LanguageSwitcher = (props: ILanguageSwitcher) => {
         defaultValue={localeActive}
         onChange={handleLanguageChange}
         disabled={isPending}
-        className={`appearance-none bg-transparent border border-gray-300 rounded-md py-2 px-4 pr-8 leading-tight focus:outline-none focus:border-gray-300
+        className={`appearance-none bg-transparent border border-gray-300 rounded-md py-2 px-4 pr-8 leading-tight focus:outline-none focus:border-gray-300 cursor-pointer
         text-xs font-semibold ${
           isHamburgerNavbar && "dark:border-dark text-light dark:text-dark"
         }`}
